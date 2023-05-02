9dbdc04d76aeb23792fa5f17265d4b55e8f7acd1
Talha Arslan <107929171+Lynch811@users.noreply.github.com>
2023-05-02 03:53:07 +0300

    Bunifu eklemeleri başarılı admin girişi yapıldı

--- TicariOtomasyon/Giris.Designer.cs
+++ TicariOtomasyon/Giris.Designer.cs
@@ -30,11 +30,302 @@ namespace TicariOtomasyon
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TxtKadi = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.TxtSifre = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.LblGirisYap = new System.Windows.Forms.Label();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton2 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton3 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton4 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuCheckbox1 = new Bunifu.Framework.UI.BunifuCheckbox();
+            this.bunifuGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(172)))), ((int)(((byte)(23)))), ((int)(((byte)(65)))));
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(438, 594);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 310);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TxtKadi
+            // 
+            this.TxtKadi.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TxtKadi.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKadi.ForeColor = System.Drawing.Color.White;
+            this.TxtKadi.HintForeColor = System.Drawing.Color.Empty;
+            this.TxtKadi.HintText = "";
+            this.TxtKadi.isPassword = false;
+            this.TxtKadi.LineFocusedColor = System.Drawing.Color.DimGray;
+            this.TxtKadi.LineIdleColor = System.Drawing.Color.Gray;
+            this.TxtKadi.LineMouseHoverColor = System.Drawing.Color.DimGray;
+            this.TxtKadi.LineThickness = 4;
+            this.TxtKadi.Location = new System.Drawing.Point(684, 190);
+            this.TxtKadi.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.TxtKadi.Name = "TxtKadi";
+            this.TxtKadi.Size = new System.Drawing.Size(374, 58);
+            this.TxtKadi.TabIndex = 1;
+            this.TxtKadi.Text = "Kullanıcı Adı";
+            this.TxtKadi.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.TxtKadi.Enter += new System.EventHandler(this.TxtKadi_Enter);
+            this.TxtKadi.Leave += new System.EventHandler(this.TxtKadi_Leave);
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TxtSifre.Font = new System.Drawing.Font("Century Gothic", 15F);
+            this.TxtSifre.ForeColor = System.Drawing.Color.White;
+            this.TxtSifre.HintForeColor = System.Drawing.Color.Empty;
+            this.TxtSifre.HintText = "";
+            this.TxtSifre.isPassword = false;
+            this.TxtSifre.LineFocusedColor = System.Drawing.Color.DimGray;
+            this.TxtSifre.LineIdleColor = System.Drawing.Color.Gray;
+            this.TxtSifre.LineMouseHoverColor = System.Drawing.Color.DimGray;
+            this.TxtSifre.LineThickness = 4;
+            this.TxtSifre.Location = new System.Drawing.Point(684, 264);
+            this.TxtSifre.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(374, 58);
+            this.TxtSifre.TabIndex = 2;
+            this.TxtSifre.Text = "Şifre";
+            this.TxtSifre.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.TxtSifre.OnValueChanged += new System.EventHandler(this.TxtSifre_OnValueChanged);
+            this.TxtSifre.Enter += new System.EventHandler(this.TxtSifre_Enter);
+            this.TxtSifre.Leave += new System.EventHandler(this.TxtSifre_Leave);
+            // 
+            // LblGirisYap
+            // 
+            this.LblGirisYap.AutoSize = true;
+            this.LblGirisYap.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblGirisYap.ForeColor = System.Drawing.Color.White;
+            this.LblGirisYap.Location = new System.Drawing.Point(783, 88);
+            this.LblGirisYap.Name = "LblGirisYap";
+            this.LblGirisYap.Size = new System.Drawing.Size(128, 33);
+            this.LblGirisYap.TabIndex = 3;
+            this.LblGirisYap.Text = "Giris Yap";
+            this.LblGirisYap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.SystemColors.ActiveBorder;
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.Transparent;
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "Giriş Yap";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.Transparent;
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.bunifuThinButton21.Location = new System.Drawing.Point(724, 360);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(277, 53);
+            this.bunifuThinButton21.TabIndex = 4;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.bunifuThinButton21_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(783, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 1);
+            this.label2.TabIndex = 5;
+            this.label2.Text = " ";
+            this.label2.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(615, 190);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 53);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(615, 264);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(724, 474);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(65, 61);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 8;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // bunifuImageButton2
+            // 
+            this.bunifuImageButton2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton2.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton2.Image")));
+            this.bunifuImageButton2.ImageActive = null;
+            this.bunifuImageButton2.Location = new System.Drawing.Point(831, 474);
+            this.bunifuImageButton2.Name = "bunifuImageButton2";
+            this.bunifuImageButton2.Size = new System.Drawing.Size(65, 61);
+            this.bunifuImageButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton2.TabIndex = 9;
+            this.bunifuImageButton2.TabStop = false;
+            this.bunifuImageButton2.Zoom = 10;
+            // 
+            // bunifuImageButton3
+            // 
+            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
+            this.bunifuImageButton3.ImageActive = null;
+            this.bunifuImageButton3.Location = new System.Drawing.Point(936, 474);
+            this.bunifuImageButton3.Name = "bunifuImageButton3";
+            this.bunifuImageButton3.Size = new System.Drawing.Size(65, 61);
+            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton3.TabIndex = 10;
+            this.bunifuImageButton3.TabStop = false;
+            this.bunifuImageButton3.Zoom = 10;
+            // 
+            // bunifuImageButton4
+            // 
+            this.bunifuImageButton4.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton4.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton4.Image")));
+            this.bunifuImageButton4.ImageActive = null;
+            this.bunifuImageButton4.Location = new System.Drawing.Point(1094, 12);
+            this.bunifuImageButton4.Name = "bunifuImageButton4";
+            this.bunifuImageButton4.Size = new System.Drawing.Size(50, 50);
+            this.bunifuImageButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bunifuImageButton4.TabIndex = 11;
+            this.bunifuImageButton4.TabStop = false;
+            this.bunifuImageButton4.Zoom = 10;
+            this.bunifuImageButton4.Click += new System.EventHandler(this.bunifuImageButton4_Click);
+            // 
+            // bunifuCheckbox1
+            // 
+            this.bunifuCheckbox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox1.ChechedOffColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(135)))), ((int)(((byte)(140)))));
+            this.bunifuCheckbox1.Checked = false;
+            this.bunifuCheckbox1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(205)))), ((int)(((byte)(117)))));
+            this.bunifuCheckbox1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCheckbox1.Location = new System.Drawing.Point(1084, 294);
+            this.bunifuCheckbox1.Name = "bunifuCheckbox1";
+            this.bunifuCheckbox1.Size = new System.Drawing.Size(20, 20);
+            this.bunifuCheckbox1.TabIndex = 12;
+            this.bunifuCheckbox1.OnChange += new System.EventHandler(this.bunifuCheckbox1_OnChange);
+            // 
+            // Giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(1156, 594);
+            this.Controls.Add(this.bunifuCheckbox1);
+            this.Controls.Add(this.bunifuImageButton4);
+            this.Controls.Add(this.bunifuImageButton3);
+            this.Controls.Add(this.bunifuImageButton2);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bunifuThinButton21);
+            this.Controls.Add(this.LblGirisYap);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.TxtKadi);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Giris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Giris";
+            this.Load += new System.EventHandler(this.Giris_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Giris_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Giris_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Giris_MouseUp);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private System.Windows.Forms.Label LblGirisYap;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox TxtSifre;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox TxtKadi;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton4;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton3;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton2;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Bunifu.Framework.UI.BunifuCheckbox bunifuCheckbox1;
     }
 }
--- TicariOtomasyon/Giris.cs
+++ TicariOtomasyon/Giris.cs
@@ -7,14 +7,143 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TicariOtomasyon
 {
     public partial class Giris : Form
     {
+        SqlBaglanti bgl = new SqlBaglanti();
         public Giris()
         {
             InitializeComponent();
         }
+
+        private void Giris_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 90;
+
+        }
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label2.Visible = true;
+        }
+
+
+        #region TXT
+
+        private void bunifuCheckbox1_OnChange(object sender, EventArgs e)
+        {
+            //checkBox işaretli ise
+            if (bunifuCheckbox1.Checked)
+            {
+                //karakteri göster.
+                TxtSifre.isPassword = false;
+            }
+            //değilse karakterlerin yerine * koy.
+            else
+            {
+                TxtSifre.isPassword = true;
+            }
+        }
+        private void TxtKadi_Enter(object sender, EventArgs e)
+        {
+            if (TxtKadi.Text == "Kullanıcı Adı")
+                TxtKadi.Text = "";
+        }
+
+        private void TxtKadi_Leave(object sender, EventArgs e)
+        {
+            if (TxtKadi.Text == "")
+                TxtKadi.Text = "Kullanıcı Adı";
+        }
+
+        private void TxtSifre_Enter(object sender, EventArgs e)
+        {
+            if (TxtSifre.Text == "Şifre")
+                TxtSifre.Text = "";
+        }
+
+        private void TxtSifre_Leave(object sender, EventArgs e)
+        {
+            if (TxtSifre.Text == "")
+                TxtSifre.Text = "Şifre";
+        }
+
+        #endregion
+
+        private void bunifuImageButton4_Click(object sender, EventArgs e)
+        {
+            DialogResult secim = new DialogResult();
+            secim = MessageBox.Show("Uygulamayı kapatmak üzeresin!", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (secim == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+                
+        }
+
+        bool move;
+        int mause_x;
+        int mause_y;
+        private void Giris_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            mause_x = e.X;
+            mause_y = e.Y;
+        }
+
+        private void Giris_MouseMove(object sender, MouseEventArgs e)
+        {
+            label2.Visible = false;
+            if (move==true)
+            {
+                this.SetDesktopLocation(MousePosition.X - mause_x, MousePosition.Y - mause_y);
+            }
+        }
+
+        private void Giris_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("SELECT * FROM TBL_ADMIN WHERE KULLANICIADI=@p1 AND SIFRE=@p2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", TxtKadi.Text);
+            komut.Parameters.AddWithValue("@p2", TxtSifre.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+                FrmAnaSayfa fr = new FrmAnaSayfa();
+                fr.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı kullanıcı adı veya şifre", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void TxtSifre_OnValueChanged(object sender, EventArgs e)
+        {
+          
+            //checkBox işaretli ise
+            if (bunifuCheckbox1.Checked)
+            {
+                //karakteri göster.
+                TxtSifre.isPassword = false;
+            }
+            //değilse karakterlerin yerine * koy.
+            else
+            {
+                TxtSifre.isPassword = true;
+            }
+        }
+
+        
     }
 }
